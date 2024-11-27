10b6a737665cdd23f49e4a9c353600dc4d199b33
adi17082001 <adi17082001@gmail.com>
2024-11-27 13:01:08 +0530

    Online Shop Template Integration

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,9 +4,10 @@ import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  // templateUrl: './product-list-table.component.html',
   // templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
   
