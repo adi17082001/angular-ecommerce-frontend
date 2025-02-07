514c7d598857b092c930fe533b7ae2ff52f38fb4
adi17082001 <adi17082001@gmail.com>
2025-02-07 11:21:33 +0530

    search for products by keyword

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { ProductService } from './services/product.service';
 
 import {Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
+  {path: 'search/:keyword', component: ProductListComponent}, // added route for search products by keyword
   {path: 'category/:id', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
@@ -23,7 +25,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
   
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false; // search for products by keyword
 
   constructor(private productService: ProductService,
           private route: ActivatedRoute){ }
@@ -24,6 +25,31 @@ export class ProductListComponent implements OnInit{
   }
   
   listProducts(){
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }
+    else{
+      this.handleListProducts();
+    }
+
+  }
+
+  // search for products by keyword
+  handleSearchProducts(){
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now need to search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+
+  handleListProducts(){
 
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
