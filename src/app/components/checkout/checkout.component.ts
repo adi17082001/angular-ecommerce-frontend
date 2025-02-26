a3831fe39441d4961757018bb83a5a126b1ce349
adi17082001 <adi17082001@gmail.com>
2025-02-26 16:44:05 +0530

    Checkout Form - validation for customer details

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
@@ -35,9 +35,11 @@ export class CheckoutComponent implements OnInit {
       
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        // new Form Control for Checkout Form Validation
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', 
+                              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -93,6 +95,11 @@ export class CheckoutComponent implements OnInit {
     
   }
 
+  // getter methods for checkout form validation
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
   // method to copy shipping address to billing address
   copyShippingAddressToBillingAddress(event: any){
     if(event.target.checked){
@@ -113,6 +120,12 @@ export class CheckoutComponent implements OnInit {
   // submit button
   onSubmit(){
     console.log("Handling form submission");
+
+    // checkout form validation
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched(); // touching all fields triggers the display of error messages
+    }
+
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
 
